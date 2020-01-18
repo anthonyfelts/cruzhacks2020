b36ddbe48ea4efeb8b28669f51158c9b6525f9d5
Anthony Felts <me@anthonyfelts.com>
2020-01-18 14:08:42 +0000

    fixed axios required headers

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const siteUrl = "https://nutrition.sa.ucsc.edu/";
+const siteUrl = "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&naFlag=1";
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const instance = axios.create({
+  // baseURL: '',
+  timeout: 1000,
+  headers: {
+    'Cookie' : 'SavedAllergens=; SavedWebCodes=; WebInaCartLocation=40; WebInaCartDates=; WebInaCartMeals=; WebInaCartRecipes=; WebInaCartQtys=',
+  }
+});
+
 const fetchData = async () => {
-    const result = await axios.get(siteUrl);
+    const result = await instance.get(siteUrl);
     return cheerio.load(result.data);
 };
 
+// fetchData();
 fetchData().then(data => console.log(data.html()));
+
+// const $ = await fetchData();
+// const postJobButton = $('.top > .action-post-job').text();
+// console.log(postJobButton) // Logs 'Post a Job'
