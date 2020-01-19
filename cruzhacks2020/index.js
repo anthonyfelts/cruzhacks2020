e12313565d157ef4f7dc8080c5cde858706ca5af
Cole Scott <me@colescott.io>
2020-01-18 23:32:09 -0800

    Add Meal dining hall intent

--- cruzhacks2020/index.js
+++ cruzhacks2020/index.js
@@ -93,7 +93,7 @@ const makeResponse = (dininghall, meal, arrayMenu) => {
   //CASE: menuMeal
   
   let menuMealResponseArray = [
-      `Today for ${meal}, ${dininghall} dining hall is serving "${prettyListString}.`,
+      `Today for ${meal}, ${dininghall} dining hall is serving ${prettyListString}.`,
       `${dininghall} dining hall is serving ${prettyListString} for ${meal} today.` //if the prettyList is too long, this is not a good response
       ]
       
@@ -115,20 +115,32 @@ module.exports = async (context, req) => {
 
   const intent = req.body.queryResult.intent.name;
   switch(intent) {
-    case "projects/dininghallagent-wxjsso/agent/intents/a7819adb-5c87-46ba-99c3-dde1fe271bca":
+    case "projects/dininghallagent-wxjsso/agent/intents/a7819adb-5c87-46ba-99c3-dde1fe271bca": {
       // DiningHallMenuCurrent
       const dh = req.body.queryResult.parameters.DiningHall;
       const menu = await menuCurrent(dh);
 
       context.res = ok(menu);
       break;
-    default:
+    }
+    case "projects/dininghallagent-wxjsso/agent/intents/1352c30e-00cd-4f4d-8211-9dd186604455": {
+      // DiningHallMenuMeal
+      const dh = req.body.queryResult.parameters.DiningHall;
+      const meal = req.body.queryResult.parameters.Meal;
+      const menu = await menuMeal(dh, meal);
+
+      context.res = ok(menu);
+
+      break;
+    }
+    default: {
       console.error(`Unable to handle intent ${intent}`);
       context.res = {
         status: 400,
         body: { fulfillmentText: "Sorry, I don't know how to respond to that." }
       };
       break;
+    }
   }
 };
 
@@ -146,7 +158,6 @@ const menuCurrent = dininghall => {
 
 const menuMeal = async (dh, meal) => {
   const menu = await getMenu(dh);
-  console.log(menu[meal]);
   return makeResponse(dh, meal, menu[meal]);
 };
 
